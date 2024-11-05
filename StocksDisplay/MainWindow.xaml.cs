29f6e2f7f4f69b4eb6cfb706ee4995135f8c0758
marcel <64838464+lis-marcel@users.noreply.github.com>
2024-11-05 13:11:13 +0100

    Initial commit

---
+++ StocksDisplay/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using StocksDisplay.Services;
+using System.Collections.Generic;
+using System.Windows;
+using StocksDisplay.Services;
+
+namespace StocksDisplay
+{
+    public partial class MainWindow : Window
+    {
+        private readonly CompanyStocksService _companyActionsService;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _companyActionsService = new();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Fetch company actions using the service
+            List<string> companyActions = _companyActionsService.GetCompanyStocks();
+
+            // Display actions in the ListBox
+            ActionsListBox.ItemsSource = companyActions;
+        }
+    }
+}
---
+++ StocksDisplay/Services/CompanyStocksService.cs
@@ -0,0 +1,17 @@
+﻿namespace StocksDisplay.Services
+{
+    public class CompanyStocksService
+    {
+        public List<string> GetCompanyStocks()
+        {
+            // This method should ideally fetch data from a database or an API
+            // For now, we'll return a static list of actions
+            return new List<string>
+            {
+                "Company A: Acquired Company B",
+                "Company C: Launched new product",
+                "Company D: Announced quarterly earnings"
+            };
+        }
+    }
+}
