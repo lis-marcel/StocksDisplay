e938dcb095bcb1282cf3cdace1fc15f450358118
marcel <lis.marcys@gmail.com>
2025-12-12 00:06:14 +0100

    [feat] added data converters

---
+++ StocksDisplay/Converters/IsPositiveConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows.Data;
+
+namespace StocksDisplay.Converters
+{
+    public class IsPositiveConverter : IValueConverter
+    {
+        public object Convert(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is double doubleValue)
+            {
+                return doubleValue > 0;
+            }
+            return false;
+        }
+        public object ConvertBack(object value, System.Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ StocksDisplay/Converters/TickerToImagePathConverter.cs
@@ -0,0 +1,49 @@
+﻿using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace StocksDisplay.Converters
+{
+    public class TickerToImagePathConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string ticker && !string.IsNullOrEmpty(ticker))
+            {
+                try
+                {
+                    var currentDir = Directory.GetCurrentDirectory();
+                    var projectPath = Directory.GetParent(currentDir)?.Parent?.Parent?.FullName;
+
+                    if (projectPath != null)
+                    {
+                        var imagePath = Path.Combine(projectPath, "Media", "Images", $"{ticker}.png");
+
+                        if (File.Exists(imagePath))
+                        {
+                            // Tworzymy BitmapImage z pliku
+                            var bitmap = new BitmapImage();
+                            bitmap.BeginInit();
+                            bitmap.UriSource = new Uri(imagePath, UriKind.Absolute);
+                            bitmap.CacheOption = BitmapCacheOption.OnLoad; // Ważne, żeby nie blokować pliku
+                            bitmap.EndInit();
+                            return bitmap;
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
